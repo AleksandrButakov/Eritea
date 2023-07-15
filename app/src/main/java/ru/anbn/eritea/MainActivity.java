2a55ca2906d179aa89a5b8ca1addbb9b12aaf69d
AleksandrButakov <anbngm@gmail.com>
2023-07-15 09:53:07 +0500

    add sms manager in code

--- app/src/main/java/ru/anbn/eritea/MainActivity.java
+++ app/src/main/java/ru/anbn/eritea/MainActivity.java
@@ -3,12 +3,38 @@ package ru.anbn.eritea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String SENT_SMS = "SENT_SMS";
+    String DELIVER_SMS = "DELIVER_SMS";
+
+    EditText address;
+    EditText text;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        address = (EditText) findViewById(R.id.address);
+        text = (EditText) findViewById(R.id.text);
+
+        button = (Button) findViewById(R.id.sendBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+
+            }
+        });
+
+
     }
 }
